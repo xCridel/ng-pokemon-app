cc51058b8bf27f8ee007e5ba03509f7d1d291f68
Christophe Delestre <christophe.delestre@outlook.com>
2024-01-16 12:13:40 +0100

    Ajout filtre RxJs

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -10,6 +10,8 @@ import { FormsModule } from "@angular/forms";
 import { PokemonFormComponent } from "./pokemon-form/pokemon-form.component";
 import { EditPokemonComponent } from "./edit-pokemon/edit-pokemon.component";
 import { AddPokemonComponent } from "./add-pokemon/add-pokemon.component";
+import { SearchPokemonComponent } from "./search-pokemon/search-pokemon.component";
+import { LoaderComponent } from './loader/loader.component';
 
 const pokemonRoutes: Routes = [
   { path: "edit/pokemon/:id", component: EditPokemonComponent },
@@ -27,6 +29,8 @@ const pokemonRoutes: Routes = [
     PokemonFormComponent,
     EditPokemonComponent,
     AddPokemonComponent,
+    SearchPokemonComponent,
+    LoaderComponent,
   ],
   imports: [CommonModule, FormsModule, RouterModule.forChild(pokemonRoutes)],
   providers: [PokemonService],
---
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from "@angular/core";
+import { Pokemon } from "../pokemon";
+import { Router } from "@angular/router";
+import { PokemonService } from "../pokemon.service";
+import {
+  Observable,
+  Subject,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+} from "rxjs";
+
+@Component({
+  selector: "app-search-pokemon",
+  templateUrl: "./search-pokemon.component.html",
+})
+export class SearchPokemonComponent implements OnInit {
+  searchTerms = new Subject<string>();
+  pokemons$: Observable<Pokemon[]>;
+
+  constructor(private router: Router, private pokemonService: PokemonService) {}
+  ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term) => this.pokemonService.searchPokemonList(term))
+    );
+  }
+
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  goToDetail(pokemon: Pokemon) {
+    const link = ["/pokemon", pokemon.id];
+    this.router.navigate(link);
+  }
+}
